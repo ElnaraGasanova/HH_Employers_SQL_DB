931710b189a6936ca8d32c2746178b5a6d26fe18
ElnaraGasanova <elnaraskypro@gmail.com>
2024-01-29 01:22:26 +0300

    Подключения к БД PostgreSQL и работа с таблицами.

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,28 +1,84 @@
+import psycopg2
+password = 'POSTGRESSQL_KEY'
+
+
 class DBManager:
     """Класс подключения и работы к БД PostgreSQL"""
 
+    @staticmethod
+    def get_companies_and_vacancies_count(self):
+        """Получает список всех компаний
+        и количество вакансий у каждой компании."""
+        with psycopg2.connect(host="localhost", database="hh_employers_db",
+                              user="postgres", password='POSTGRESSQL_KEY', encoding='utf-8') as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT company_name, COUNT(vacancies_name) AS count_vacancies  "
+                            f"FROM employers "
+                            f"JOIN vacancies USING (employer_id) "
+                            f"GROUP BY employers.company_name")
+                result = cur.fetchall()
+            conn.commit()
+        return result
 
-def get_companies_and_vacancies_count():
-    """Получает список всех компаний
-    и количество вакансий у каждой компании."""
-    pass
+    @staticmethod
+    def get_all_vacancies(self):
+        """Получает список всех вакансий с указанием
+        названия компании, названия вакансии и зарплаты
+        и ссылки на вакансию."""
+        with psycopg2.connect(host="localhost", database="hh_employers_db",
+                              user="postgres", password='POSTGRESSQL_KEY', encoding='utf-8') as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT employers.company_name, vacancies.vacancies_name, "
+                            f"vacancies.payment, vacancies_url "
+                            f"FROM employers "
+                            f"JOIN vacancies USING (employer_id)")
+                result = cur.fetchall()
+            conn.commit()
+        return result
 
-def get_all_vacancies():
-    """Получает список всех вакансий с указанием
-    названия компании, названия вакансии и зарплаты
-    и ссылки на вакансию."""
-    pass
+    @staticmethod
+    def get_avg_salary(self):
+        """Получает среднюю зарплату по вакансиям."""
+        with psycopg2.connect(host="localhost", database="hh_employers_db",
+                              user="postgres", password='POSTGRESSQL_KEY', encoding='utf-8') as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT AVG(payment) as avg_payment FROM vacancies ")
+                result = cur.fetchall()
+            conn.commit()
+        return result
 
-def get_avg_salary():
-    """Получает среднюю зарплату по вакансиям."""
-    pass
+    @staticmethod
+    def get_vacancies_with_higher_salary(self):
+        """Получает список всех вакансий, у которых
+        зарплата выше средней по всем вакансиям."""
+        with psycopg2.connect(host="localhost", database="hh_employers_db",
+                              user="postgres", password='POSTGRESSQL_KEY', encoding='utf-8') as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT * FROM vacancies "
+                            f"WHERE payment > (SELECT AVG(payment) FROM vacancies) ")
+                result = cur.fetchall()
+            conn.commit()
+        return result
 
-def get_vacancies_with_higher_salary():
-    """Получает список всех вакансий, у которых
-    зарплата выше средней по всем вакансиям."""
-    pass
+    @staticmethod
+    def get_vacancies_with_keyword(self, keyword):
+        """Получает список всех вакансий, в названии которых
+        содержатся переданные в метод слова, например python."""
+        with psycopg2.connect(host="localhost", database="hh_employers_db",
+                              user="postgres", password='POSTGRESSQL_KEY', encoding='utf-8') as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT * FROM vacancies "
+                            f"WHERE lower(vacancies_name) LIKE '%{keyword}%' "
+                            f"OR lower(vacancies_name) LIKE '%{keyword}'"
+                            f"OR lower(vacancies_name) LIKE '{keyword}%';")
+                result = cur.fetchall()
+            conn.commit()
+        return result
 
-def get_vacancies_with_keyword():
-    """Получает список всех вакансий, в названии которых
-    содержатся переданные в метод слова, например python."""
-    pass
+
+def format_salary(salary_from, salary_to):
+    if salary_from is None:
+        salary_from = 'не указано'
+    if salary_to is None:
+        salary_to = 'не указано'
+    return salary_from, salary_to
