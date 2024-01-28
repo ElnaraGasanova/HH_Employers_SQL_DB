ee140ea451ad20718fca703157cb46842bdb0a18
ElnaraGasanova <elnaraskypro@gmail.com>
2024-01-28 12:46:22 +0300

    Набросок функций.

---
+++ utils/utils.py
@@ -0,0 +1,23 @@
+import psycopg2
+import requests
+
+
+def get_employer():
+    """Получение данных c HH о работодателе."""
+    pass
+
+
+def get_vacancies():
+    """Получение данных c HH о вакансиях работодателя."""
+    pass
+
+
+def create_table():
+    """SQL. Создание БД и таблиц."""
+    pass
+
+
+def fill_in_table():
+    """SQL. Заполнение таблиц данными с HH."""
+    pass
+
